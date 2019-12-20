7b5a2d6199f635820bac9384304c94a7df56116b
jinnrw <Jinn@JinndeMBP.hitronhub.home>
2019-12-20 00:02:43 -0800

    Add express server, integrate with react, migrate data into server

---
+++ app.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const path = require('path');
+
+
+// Init App
+const app = express();
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+})
+
+const data = { title: "My Trello App" };
+
+// GET method route
+app.get('/api', (req, res) => {
+  res.json(data);
+})
+
+const lists = [{
+  list_title: "To Do",
+  list_items: [
+    {
+      title: "do this",
+      completed: false,
+    },
+    {
+      title: "do that",
+      completed: false,
+    },
+    {
+      title: "push to github",
+      completed: false,
+    }
+  ]
+}, {
+  list_title: "Done",
+  list_items: [
+    {
+      title: "buy milk",
+      completed: false,
+    },
+    {
+      title: "overcooked dinner",
+      completed: false,
+    },
+    {
+      title: "save your code",
+      completed: false,
+    }
+  ]
+}
+]
+
+app.get('/api/lists', (req, res) => {
+  res.json(lists);
+})
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ client/build/precache-manifest.fc4c42320b594c0f73d5fcd0321a79d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d330451214a9a8caa62fbfdb39021db5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e27e4b7738e93c8772f",
+    "url": "/static/css/main.0d4505bd.chunk.css"
+  },
+  {
+    "revision": "6119f4e48d05fd4b2df5",
+    "url": "/static/js/2.43cd084a.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/static/js/2.43cd084a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1e27e4b7738e93c8772f",
+    "url": "/static/js/main.8c81d46d.chunk.js"
+  },
+  {
+    "revision": "7df52723d36a62aa5396",
+    "url": "/static/js/runtime-main.3127cda7.js"
+  }
+]);
